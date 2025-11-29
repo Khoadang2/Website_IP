c6370d1866ffbdcb555de0488dae2bcd58a8a2bd
KayIT <dangkhoabui72@gmail.com>
2025-11-29 08:30:28 +0700

    first commit

---
+++ backend/db.js
@@ -0,0 +1,57 @@
+// db.js
+const sql = require("mssql");
+
+// Kết nối DB WEB_IP
+const configWEB = {
+  user: "sa",
+  password: "IT@Admin17",
+  server: "192.168.71.5",
+  database: "WEB_IP",
+  options: {
+    encrypt: false,
+    trustServerCertificate: true
+  }
+};
+
+// Kết nối DB LYS_ERP (cho trang đăng nhập)
+const configLogin = {
+  user: "tyxuan",
+  password: "jack",
+  server: "192.168.71.7",
+  database: "LYS_ERP",   // 🔹 sửa lại đúng database
+  options: {
+    encrypt: false,
+    trustServerCertificate: true
+  }
+};
+
+
+// Pool WEB_IP
+const poolWEB = new sql.ConnectionPool(configWEB)
+  .connect()
+  .then(pool => {
+    console.log("✅ Kết nối SQL Server (WEB_IP) thành công");
+    return pool;
+  })
+  .catch(err => {
+    console.error("❌ Lỗi kết nối SQL Server (WEB_IP):", err);
+    process.exit(1);
+  });
+
+// Pool LIY_TYTHAC
+const poolLogin = new sql.ConnectionPool(configLogin)
+  .connect()
+  .then(pool => {
+    console.log("✅ Kết nối SQL Server (LIY_TYTHAC) thành công");
+    return pool;
+  })
+  .catch(err => {
+    console.error("❌ Lỗi kết nối SQL Server (LIY_TYTHAC):", err);
+    process.exit(1);
+  });
+
+module.exports = {
+  sql,
+  poolWEB,
+  poolLogin
+};
